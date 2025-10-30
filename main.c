1cbaca86ac0b65de2ef81edd8ff84610d6cc55be
dutnals <dutnaks77@sookmyung.ac.kr>
2025-10-30 16:14:16 +0900

    03_2

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 int main(void)
 {
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
-	
-	for (i=0; i<SIZE; i++)
-		grade[i] = rand() % 100; 
-
-	for( i=0; i<SIZE; i++)
-		score[i] = grade[i];
-	
-	for( i=0; i<SIZE; i++)
-		printf("grade[%d] = %d, score[%d] = %d\n", i, grade[i], i, score[i]);
+	int a[SIZE] = {1, 0, 3, 4, 5};
+	int b[SIZE] = {1, 2, 3, 4, 5}; 
+	int flag_same = 1; 
 		
+	for( i=0; i<SIZE; i++){
+		if (a[i] != b[i]){
+			printf("Not same at %dth element!\n", i);
+			flag_same = 0;
+		}
+	}
+	
+	printf("Are a and b the same? %d", flag_same);
+	
 	return 0;
 }
